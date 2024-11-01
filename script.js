d87b05a40a8b915f9411cb260b69e5f5e8517e8c
kdaui <78982505+kdaui@users.noreply.github.com>
2024-11-01 12:21:09 +0000

    Added tres filos

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.pirate-btn').addEventListener('click', () => {
+    window.location.href = 'p-t.html';
+});
+
+document.querySelector('.manga-btn').addEventListener('click', () => {
+    window.location.href = 'm-t.html';
+});
+
+document.querySelector('.atprotocol-btn').addEventListener('click', () => {
+    window.location.href = 'a-t.html';
+});
